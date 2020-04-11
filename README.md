b5d4ef1b0e04b9845e2e8f582a0275f38a59df0d
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2020-04-11 10:27:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# instrumentserver
+
+a tool for managing qcodes in a server environment
+
+For installing, use a developer pip install:
+```
+pip install --no-deps -e /folder/to/instrumentserver/
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='instrumentserver',
+      version='0.0.1',
+      description='TBD',
+      url='https://github.com/toolsforexperiments/instrumentserver',
+      author='Wolfgang Pfaff',
+      author_email='wolfgangpfff@gmail.com',
+      license='MIT',
+      packages=['instrumentserver'],
+      zip_safe=False)
