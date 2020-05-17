3b95484228212ff6f08afbdf38c38ebc02e9501a
hatlabcz <chz78@pitt.edu>
2020-05-17 00:53:08 -0400

    Clean up. Ready to merge to the upstream

--- test_proxy.py
+++ test_proxy.py
@@ -6,9 +6,6 @@ Created on Sat Apr 18 22:12:05 2020
 """
 
 from instrumentserver.proxy import InstrumentProxy, create_instrument, get_existing_instruments
-import qcodes
-import json 
-import jsonpickle
 
 rf_src = InstrumentProxy('rf_src')
 dummy_vna = InstrumentProxy('dummy_vna')
