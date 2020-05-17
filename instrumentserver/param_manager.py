a40b22bdface8dd9a0111c4358062db85214b0a9
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2020-05-17 15:06:53 +0200

    fix: avoid naming conflicts by giving submodules a unique name. added a method to clean up empty submodules after removing params.

--- instrumentserver/param_manager.py
+++ instrumentserver/param_manager.py
@@ -20,15 +20,19 @@ class ParameterManager(Instrument):
         parent = self._get_parent(param_name)
         return parent.parameters[param_name.split('.')[-1]]
 
-    def _get_parent(self, param_name: str, create_parent: bool = False) -> Parameter:
+    def _get_parent(self, param_name: str, create_parent: bool = False) \
+            -> 'ParameterManager':
+
         split_names = param_name.split('.')
         parent = self
+        full_name = self.name
         for i, n in enumerate(split_names[:-1]):
+            full_name += f'.{n}'
             if n in parent.parameters:
                 raise ValueError(f"{n} is a parameter, and cannot have child parameters.")
             if n not in parent.submodules:
                 if create_parent:
-                    parent.add_submodule(n, ParameterManager(n))
+                    parent.add_submodule(n, ParameterManager(full_name))
                 else:
                     raise ValueError(f'{n} does not exist.')
             parent = parent.submodules[n]
@@ -60,10 +64,12 @@ class ParameterManager(Instrument):
         parent = self._get_parent(param_name, create_parent=True)
         parent.add_parameter(param_name.split('.')[-1], **kw)
 
-    def remove(self, param_name: str):
+    def remove(self, param_name: str, cleanup: bool = True):
         parent = self._get_parent(param_name)
         pname = param_name.split('.')[-1]
         del parent.parameters[pname]
+        if cleanup:
+            self.remove_empty_submodules()
 
     def get(self, param_name: str) -> Any:
         param = self._get_param(param_name)
@@ -74,6 +80,22 @@ class ParameterManager(Instrument):
         param.set(value)
 
     def remove_empty_submodules(self):
-        raise NotImplementedError
+        """delete all empty submodules in the instrument."""
 
+        def is_empty(parent):
+            if len(parent.submodules) == 0 and len(parent.parameters) == 0:
+                return True
+            else:
+                return False
 
+        def purge(parent):
+            mark_for_deletion = []
+            for n, s in parent.submodules.items():
+                purge(s)
+                if is_empty(s):
+                    mark_for_deletion.append(n)
+            for n in mark_for_deletion:
+                parent.submodules[n].close()
+                del parent.submodules[n]
+
+        purge(self)
