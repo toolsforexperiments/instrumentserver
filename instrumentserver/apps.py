043b05a80d3202d93ba475f7d722a05dc42ce702
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2021-02-28 19:58:21 -0600

    moved launching script into package.

---
+++ instrumentserver/apps.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+import logging
+
+from . import QtWidgets, QtCore
+from .log import setupLogging
+from .server.application import startServerGuiApplication
+from .server.core import startServer
+
+setupLogging(addStreamHandler=True,
+             logFile=os.path.abspath('instrumentserver.log'))
+logger = logging.getLogger('instrumentserver')
+logger.setLevel(logging.DEBUG)
+
+
+def server(port, user_shutdown):
+    app = QtCore.QCoreApplication([])
+    server, thread = startServer(port, user_shutdown)
+    thread.finished.connect(app.quit)
+    return app.exec_()
+
+
+def serverWithGui(port):
+    app = QtWidgets.QApplication([])
+    window = startServerGuiApplication(port)
+    return app.exec_()
+
+
+def serverScript() -> None:
+    parser = argparse.ArgumentParser(description='Starting the instrumentserver')
+    parser.add_argument("--port", default=5555)
+    parser.add_argument("--gui", default=False)
+    parser.add_argument("--allow_user_shutdown", default=False)
+    args = parser.parse_args()
+
+    if args.gui:
+        serverWithGui(args.port)
+    else:
+        server(args.port, args.allow_user_shutdown)
