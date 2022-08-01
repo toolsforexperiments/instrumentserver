90807ff35ea6459cac20f02a2cb1df9dcc51b379
zelongx2 <zelongx2@illinois.edu>
2022-08-01 17:20:24 -0500

    Fix the dict config.

--- doc/dashboard_example.py
+++ doc/dashboard_example.py
@@ -5,55 +5,56 @@ the documentation on the dashboard.
 
 
 config = {
-    'Random Numbers 1': {
-        'Param 0': {
-            'source_type': 'parameter',
-            'parameter_path': 'test.param0',
-            'server': 'localhost',
-            'port': 5555,
-            'options': {
-                'interval': 1
-            }
-         },
-        'Param 1': {
-            'source_type': 'parameter',
-            'parameter_path': 'test.param1',
-            'server': 'localhost',
-            'port': 5555,
-            'options': {
-                'interval': 1
-            }
-        },
-        'Param 4': {
-            'source_type': 'parameter',
-            'parameter_path': 'test.param4',
-            'server': 'localhost',
-            'port': 5555,
-            'options': {
-                'interval': 1
-            }
-        },
+    'plots': {
+        'Random Numbers 1': {
+            'Param 0': {
+                'source_type': 'parameter',
+                'parameter_path': 'test.param0',
+                'server': 'localhost',
+                'port': 5555,
+                'options': {
+                    'interval': 1
+                }
+             },
+            'Param 1': {
+                'source_type': 'parameter',
+                'parameter_path': 'test.param1',
+                'server': 'localhost',
+                'port': 5555,
+                'options': {
+                    'interval': 1
+                }
+            },
+            'Param 4': {
+                'source_type': 'parameter',
+                'parameter_path': 'test.param4',
+                'server': 'localhost',
+                'port': 5555,
+                'options': {
+                    'interval': 1
+                }
+            },
 
-        'Param 2': {
-            'source_type': 'parameter',
-            'parameter_path': 'test.param2',
-            'server': 'localhost',
-            'port': 5555,
-            'options': {
-                'interval': 1
+            'Param 2': {
+                'source_type': 'parameter',
+                'parameter_path': 'test.param2',
+                'server': 'localhost',
+                'port': 5555,
+                'options': {
+                    'interval': 1
+                }
+            },
+            'Param 3': {
+                'source_type': 'parameter',
+                'parameter_path': 'test.param3',
+                'server': 'localhost',
+                'port': 5555,
+                'options': {
+                    'interval': 1
+                }
             }
         },
-        'Param 3': {
-            'source_type': 'parameter',
-            'parameter_path': 'test.param3',
-            'server': 'localhost',
-            'port': 5555,
-            'options': {
-                'interval': 1
-            }
-        }
     },
-
     'options': {
         'refresh_rate': 10,
         'allowed_ip': ['*'],
