4916ea40138bc8f4af3c080000922f6f08dd1979
Jonathan Nolan <jmnolan5@illinois.edu>
2025-02-22 20:59:35 -0600

    updated functionality for multiple addresses

--- instrumentserver/monitoring/listener.py
+++ instrumentserver/monitoring/listener.py
@@ -20,20 +20,25 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 class Listener(ABC):
-    def __init__(self, addr: str):
-        self.addr = addr     
+    def __init__(self, addresses: list):
+        self.addresses = addresses     
 
     def run(self):
 
         # creates zmq subscriber at specified address
-        logger.info(f"Connecting to {self.addr}")
+        logger.info(f"Connecting to {self.addresses}")
         context = zmq.Context()
         socket = context.socket(zmq.SUB)
-        socket.connect(self.addr)
 
-        # listen for everything
-        socket.setsockopt_string(zmq.SUBSCRIBE, "")
-        logger.info("Listener Connected")
+        # listen at all the addresses
+        for addr in self.addresses:
+            socket.connect(addr)
+
+            # listen for everything
+            socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+            logger.info(f"Listener Connected to {addr}")
+
         listen = True
 
         try:
@@ -41,7 +46,7 @@ class Listener(ABC):
                 try:
                     # parses string message and decodes into ParameterBroadcastBluePrint
                     message = recvMultipart(socket)
-                    self.listenerEvent(message[1])
+                    self.listenerEvent(message[0],message[1])
                 except (KeyboardInterrupt, SystemExit):
                     # exit if keyboard interrupt
                     logger.info("Program Stopped Manually")
@@ -54,9 +59,9 @@ class Listener(ABC):
         pass
 
 class DFListener(Listener):
-    def __init__(self, addr: str, paramList: list, path: str):
-        super().__init__(addr)
-        self.addr = addr
+    def __init__(self, addresses: list, paramList: list, path: str):
+        super().__init__(addresses)
+        self.addresses = addresses
         self.path = path
 
         # checks if data file already exists
@@ -86,16 +91,16 @@ class DFListener(Listener):
 
 class InfluxListener(Listener):
 
-    def __init__(self, addr: str, paramList: list, token: str, org: str, bucket: str, url: str, measurementName: str):
-        super().__init__(addr)
+    def __init__(self, addresses: list, paramList: list, token: str, org: str, bucketDict: Dict[str,str], url: str, measurementNameDict: Dict[str,str]):
+        super().__init__(addresses)
 
-        self.addr = addr
+        self.addresses = addresses
         self.token = token
         self.org = org
-        self.bucket = bucket
+        self.bucketDict = bucketDict
         self.url = url
         self.paramList = list(paramList)
-        self.measurementName = measurementName
+        self.measurementNameDict = measurementNameDict
 
         self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org)
         self.write_api = self.client.write_api(write_options=WriteOptions(batch_size=1))
@@ -106,25 +111,25 @@ class InfluxListener(Listener):
     def run(self):
         super().run()
 
-    def listenerEvent(self, message: ParameterBroadcastBluePrint):
-        
+    def listenerEvent(self, instrument, message: ParameterBroadcastBluePrint):
+        bucket = self.bucketDict[instrument]
+        measurementName = self.measurementNameDict[instrument]
         # listens only for parameters in the list, if it is empty, it listens to everything
-
         if not self.paramList or message.name in self.paramList:
             logger.info(f"Writing data [{message.name},{message.value},{message.unit}]")
-            point = Point(self.measurementName).tag("name", message.name)
+            point = Point(measurementName).tag("name", message.name)
             try :
                 point = point.field("value", float(message.value))
             except ValueError:
                 point = point.field("value_string", message.value)
             point = point.time(datetime.now(self.timezoneInfo))
-            self.write_api.write(bucket=self.bucket, org=self.org, record=point)
+            self.write_api.write(bucket=bucket, org=self.org, record=point)
 
 
 def checkInfluxConfig(configInput: Dict[str, Any]):
 
     # check if all fields are present in the config file
-    influxFields = ['address', 'params', 'token', 'org', 'bucket', 'url']
+    influxFields = ['addresses', 'params', 'token', 'org', 'bucketDict', 'url', 'measurementNameDict']
     for field in influxFields:
         if field not in configInput or configInput[field] is None:
             logger.info(f"Missing field {field} in config file")
@@ -136,7 +141,7 @@ def checkInfluxConfig(configInput: Dict[str, Any]):
 def checkCSVConfig(configInput: Dict[str, Any]):
 
     # check if all fields are present in the config file
-    csvField = ["address", "params", "csv_path"]
+    csvField = ["addresses", "params", "csv_path"]
     for field in csvField:
         if field not in configInput or configInput[field] is None:
             logger.info(f"Missing field {field} in config file")
@@ -163,11 +168,11 @@ def startListener():
     if 'type' in configInput: 
         if configInput['type'] == "CSV":
             if checkCSVConfig(configInput):
-                CSVListener = DFListener(configInput['address'], configInput['params'], configInput['csv_path'])
+                CSVListener = DFListener(configInput['addresses'], configInput['params'], configInput['csv_path'])
                 CSVListener.run()
         elif configInput['type'] == "Influx": 
             if checkInfluxConfig(configInput):
-                DBListener = InfluxListener(configInput['address'], configInput['params'], configInput['token'], configInput['org'], configInput['bucket'], configInput['url'], configInput['measurementName'])
+                DBListener = InfluxListener(configInput['addresses'], configInput['params'], configInput['token'], configInput['org'], configInput['bucketDict'], configInput['url'], configInput['measurementNameDict'])
                 DBListener.run()
         else:
             logger.warning(f"Type {configInput['type']} not supported")
