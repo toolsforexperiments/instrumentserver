9f084da22a768c50207484b4435a943da2062e7f
Jonathan Nolan <jmnolan5@illinois.edu>
2025-06-30 13:34:27 -0500

    added crash prevention to pollingworker

--- instrumentserver/server/pollingWorker.py
+++ instrumentserver/server/pollingWorker.py
@@ -13,17 +13,22 @@ class PollingWorker(QtCore.QThread):
     def __init__(self, pollingRates: Optional[Dict[str, int]]=None):
         super().__init__(None)
         # This worker is supposed to only run through the server itself so there is no need to change the defaults of the client.
-        self.cli = Client()
+        self.cli = Client(raise_exceptions=False,timeout=60000)  # Don't raise exceptions on timeouts
         self.pollingRates = pollingRates
 
     # Used by the qtimers, get value of the param
     def getParamValue(self, paramName):
-        parts = paramName.split(".")
-        instr = self.cli.find_or_create_instrument(parts[0])
-        param = parts[1]
-        for part in parts[2:]:
-            param = param + "." + part
-        logger.info(f"{paramName} currently has value {nestedAttributeFromString(instr, param)()}.")
+        try:
+            parts = paramName.split(".")
+            instr = self.cli.find_or_create_instrument(parts[0])
+            param = parts[1]
+            for part in parts[2:]:
+                param = param + "." + part
+            value = nestedAttributeFromString(instr, param)()
+            logger.info(f"{paramName} currently has value {value}.")
+        except Exception as e:
+            logger.warning(f"Failed to get value for {paramName}: {str(e)}")
+            # Don't re-raise the exception, just log it and continue
 
     # Creates a qtimer for each param in the dict with the interval specified
     def run(self):
